4d9c119e41f0a201c3c488c039c42788bd1401a7
Diegom-95 <diegomaggioni95@gmail.com>
2025-03-31 18:47:28 +0200

    started project

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+import { addizione, sottrazione, moltiplicazione } from './functions.js';
+import { Persona } from './Persona.js';
+console.log(addizione(5, 5));
+console.log(sottrazione(5, 5));
+console.log(moltiplicazione(5, 5));
+const persona1 = new Persona();
+persona1.saluta();
